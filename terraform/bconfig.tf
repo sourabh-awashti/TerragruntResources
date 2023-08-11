c8d6bcc7e3d9a18ab17169d6ef5cacb8e7fe0e78
sourabh-awashti <sourabh.awashti@harness.io>
2023-08-11 14:16:20 +0530

    Create bconfig.tf

---
+++ terraform/bconfig.tf
@@ -0,0 +1,4 @@
+bucket="<+serviceVariables.bucket>"
+key="ng-test.$tfstate"
+access_key="<+secrets.getValue("account.tf_aws_400_access_key")>"
+secret_key="<+secrets.getValue("account.tf_aws_400_secret_key")>"
